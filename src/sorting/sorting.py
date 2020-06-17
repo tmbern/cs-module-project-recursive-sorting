7d927f1ab13c331b7ffadd0aa49a74a38bf9b8f3
tmbern <tmbern42@gmail.com>
2020-06-17 10:03:50 -0600

    completed tests for merge sort

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,25 +4,50 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-    i = 0
-    j = 0
+    # given two sorted arrays combine them into one sorted array.
+    # compare teh first elements of each array. 
+    # the smalles element gets added to the merged array.
+    # move the pointer on the array that had its element added
+    # and compare the next two elements. Continue adding elements 
+    # to the merged array until we have moved thru all elements in the original array.
+    # i = 0
+    # j = 0
 
-    while (i < len(arrA)) and (j < len(arrB)):
-        if arrA[i] < arrB[j]:
-            merged_arr.append(arrA[i])
-            i += 1
-        else:
-            merged_arr.append(arrB[j])
-            j += 1
+    # while (i < len(arrA)) and (j < len(arrB)):
+    #     if arrA[i] < arrB[j]:
+    #         merged_arr.append(arrA[i])
+    #         i += 1
+    #     else:
+    #         merged_arr.append(arrB[j])
+    #         j += 1
 
-    while i < len(arrA):
-        merged_arr.append(arrA[i])
-        i += 1
+    # while i < len(arrA):
+    #     merged_arr.append(arrA[i])
+    #     i += 1
     
-    while j < len(arrB):
-        merged_arr.append(arrB[j])
-        i += 1
+    # while j < len(arrB):
+    #     merged_arr.append(arrB[j])
+    #     i += 1
 
+    a = 0
+    b = 0
+
+    for i in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+
+        elif arrA[a] <= arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
 
     return merged_arr
 
@@ -34,14 +59,12 @@ def merge_sort(arr):
     # otherwise we need to divide the array in two halves and run the merge function
     # on the two halves. We will also need to recursively call the merge sort on those merged array.
 
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left_arr = arr[:mid]
-    right_arr = arr[mid:]
-    return merge(merge_sort(left_arr), merge_sort(right_arr))
-
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_arr = merge_sort(arr[:mid])
+        right_arr = merge_sort(arr[mid:])
+        arr = merge(left_arr, right_arr)
+    return arr
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
